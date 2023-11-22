561f213d37164e89821acbb8db342a52608af147
Josh Esrig <esrig@wisc.edu>
2023-11-22 15:44:17 -0600

    navbar created, optimized for smaller devices

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,21 @@
-import "./Navbar.css"
+import "./Navbar.css";
 
-import React from 'react'
-import { Link } from "react-router-dom"
+import React, { useState} from 'react';
+import { Link } from "react-router-dom";
+
+import {FaBars, FaTimes} from "react-icons/fa";
 
 const Navbar = () => {
+
+    const[click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+
   return (
     <div className="header">
       <Link to="/">
         <h1>Portfolio</h1>
       </Link>
-      <ul className="nav">
+      <ul className={click ? "nav active" : "nav"}>
         <li>
             <Link to = "/">Home</Link>
         </li>
@@ -23,9 +29,16 @@ const Navbar = () => {
             <Link to = "/about">Contact</Link>
         </li>
       </ul>
+      <div className="hamburger" onClick = {handleClick}>
+        {click ? (
+        <FaTimes size={20} style={{color: "white"}}/> 
+        ) : ( 
+        <FaBars size={20} style={{color: "white"}}/>
+       )};
     </div>
-  )
-}
+    </div>
+  );
+};
 
 export default Navbar
 
