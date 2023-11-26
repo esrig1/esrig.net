d2cbcc82d4c684c70ea940dab0489d03e699ef7e
Josh Esrig <esrig@wisc.edu>
2023-11-26 09:15:14 -0600

    about me face section added

--- src/components/about/AboutMe.js
+++ src/components/about/AboutMe.js
@@ -1,4 +1,3 @@
-// AboutMe.js
 import React from 'react';
 import myImage from '../../assets/EsrigPompeii.png';
 import './AboutMe.css';
@@ -7,10 +6,11 @@ const AboutMe = () => {
   return (
     <div className="about-me-container">
       <div className="image-container">
+        <div className='background-shape'></div>
         <img src={myImage} alt="Myself" />
       </div>
       <div className="description-container">
-        <h2>About Me</h2>
+        <h1>About Me</h1>
         <p>
           Hello! My name is [Your Name], and I am a passionate software engineer with a love for front-end development.
           I enjoy creating responsive and user-friendly interfaces. In my free time, you can find me exploring new technologies
