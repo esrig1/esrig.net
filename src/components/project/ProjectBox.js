b03311ac779dc8804bef47332c589fc4ebfd875b
Josh Esrig <esrig@wisc.edu>
2023-11-28 21:05:36 -0600

    removed link which caused bug for text to not be centered in mobile devices

--- src/components/project/ProjectBox.js
+++ src/components/project/ProjectBox.js
@@ -1,19 +1,22 @@
 // ProjectBox.js
-import React from 'react';
+import React, { useState } from 'react';
 import './ProjectBox.css'; // Import your CSS file for styling
 
 const ProjectBox = ({ title, description, demoLink, techStackImages }) => {
+
+  const handleProjectBoxClick = () => {
+    window.open(demoLink, '_blank');
+  };
+
   return (
-    <a href={demoLink} target="_blank" rel="noopener noreferrer" className="project-box-link">
-      <div className="project-box">
-        <h2>{title}</h2>
-        <p className='description'>{description}</p>
-        <p className='technologies'><strong>Technologies:</strong></p>
-        <div className="project-tech-stack">
-          {techStackImages}
-        </div>
+    <div className="project-box" onClick={handleProjectBoxClick}>
+      <h2>{title}</h2>
+      <p className='description'>{description}</p>
+      <p className='technologies'><strong>Technologies:</strong></p>
+      <div className="project-tech-stack">
+        {techStackImages}
       </div>
-    </a>
+    </div>
   );
 };
 
