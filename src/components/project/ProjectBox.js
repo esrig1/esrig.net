608ba4cf229b38dee2bb785322fb03d05e5b95ca
Josh Esrig <esrig@wisc.edu>
2023-11-28 21:01:14 -0600

    save for now

--- src/components/project/ProjectBox.js
+++ src/components/project/ProjectBox.js
@@ -8,7 +8,7 @@ const ProjectBox = ({ title, description, demoLink, techStackImages }) => {
       <div className="project-box">
         <h2>{title}</h2>
         <p className='description'>{description}</p>
-        <p><strong>Technologies:</strong></p>
+        <p className='technologies'><strong>Technologies:</strong></p>
         <div className="project-tech-stack">
           {techStackImages}
         </div>
--- src/components/project/projectSections/FrontendProjects.js
+++ src/components/project/projectSections/FrontendProjects.js
@@ -4,24 +4,21 @@ import "./XYZProjects.css"
 
 const FrontendProjects = () => {
   const techStackImages1 = [
-      <img key="1" src='/TechStackIcons/spring.svg' alt='Spring Framework' />,
-      <img key="2" src='/TechStackIcons/spring.svg' alt='Spring Framework' />,
-      <img key="3" src='/TechStackIcons/spring.svg' alt='Spring Framework' />,
-      <img key="4" src='/TechStackIcons/spring.svg' alt='Spring Framework' />
-
+      <img src='/TechStackIcons/react.svg' alt='React' />,
   ];
 
   const techStackImages2 = [
-    <img key="1" src='/TechStackIcons/spring.svg' alt='Spring Framework' />,
-    <img key="2" src='/TechStackIcons/kubernetes.svg' alt='Spring Framework' />,
-    <img key="3" src='/TechStackIcons/spring.svg' alt='Spring Framework' />,
-    <img key="4" src='/TechStackIcons/java.svg' alt='Spring Framework' />,
-    <img key="4" src='/TechStackIcons/java.svg' alt='Spring Framework' />
+    <img src='/TechStackIcons/css-3.svg' alt='CSS' />,
+    <img src='/TechStackIcons/html-5.svg' alt='HTML' />,
+    <img src='/TechStackIcons/php.svg' alt='PHP' />,
+    <img src='/TechStackIcons/mysql.svg' alt='MySQL' />,
 ];
 
 const techStackImages3 = [
-  <img key="4" src='/TechStackIcons/java.svg' alt='Spring Framework' />,
-  <img key="4" src='/TechStackIcons/java.svg' alt='Spring Framework' />
+  <img src='/TechStackIcons/css-3.svg' alt='HTML' />,
+  <img src='/TechStackIcons/html-5.svg' alt='CSS' />,
+  <img src='/TechStackIcons/javascript.svg' alt='JavaScript' />,
+
 ];
 
 
@@ -30,20 +27,26 @@ const techStackImages3 = [
      <h1>Frontend Projects</h1> 
     <div className='project-boxes'>
       <ProjectBox
-        title="Project 1"
-        description="A description of Project 1."
+        title="This Website! (esrig.net 2.0)"
+        description="This website is a responsive website written to showcase my programming and software
+        Engineering history. It is periodicall updated such that it remains consistant with my current 
+        knowledge"
         demoLink="https://example.com/project1-demo"
         techStackImages={techStackImages1}
       />
       <ProjectBox
-        title="Project 2"
-        description="A description of Project 1."
+        title="Harvard IAT Remake"
+        description="This project I created in my coding ethics class, where we studied the impact code and algorithms can have on
+        your implicit bias. This website is a reboot of Harvard's Implicit Association Test, where your attitudes and beliefs towards
+        certain groups are measured based on a survey"
         demoLink="https://example.com/project1-demo"
         techStackImages={techStackImages2}
       />
       <ProjectBox
-        title="Project 3"
-        description="A description of Project adfjlad;sfjasdkf;kljasdj;fka;sjdfjasdadfadsfasdff"
+        title="esrig.net 1.0"
+        description="The first iteration of my personal portfolio, that was created without the use of any external libraries. It only
+        utilizes the main 3 frontend languages, which was a great learning experiences with familiarizing myself with the basics of 
+        frontend"
         demoLink="https://example.com/project1-demo"
         techStackImages={techStackImages3}
       />
