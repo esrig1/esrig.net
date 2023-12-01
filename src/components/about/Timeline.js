be6e5262dd22de5e92f1f5113152950a688eb243
esrig1 <joshua.esrig@gmail.com>
2023-11-30 20:11:19 -0600

    font size moved around on timeline

--- src/components/about/Timeline.js
+++ src/components/about/Timeline.js
@@ -12,14 +12,50 @@ const Timeline = () => {
                         <FaBriefcase/>
                 </div>
                 <p className="tl-duration">
-                    May 2022 - present
+                    Feb 2023
                  </p>
-                <h5>Programming Tutor<span> - Diamond Tutoring</span></h5>
-                    <p>
-                        Diamond Tutoring is a completely virtual tutoring company that acts as a middleman between tutors and students in need.
-                        I create individual tutoring curriculums adapted specifically for each of my students' age and skill level.
-                        Scratch and Java are the primary languages that I tutor to my students.
+                <h5>Incoming Software Engineer<span> - Kohl's Technology</span></h5>
+                    <ul>
+                        <li>Coming Soon!</li>
+                    </ul>
+                </div>
+
+                <div className="timeline-item">
+                    <div className="tl-icon">
+                        <FaBriefcase/>
+                    </div>
+                    <p className="tl-duration">
+                        August 2023 - December 2023
                     </p>
+                    <h5>UW-Madison CS Capstone Co-op<span> - LastLock</span></h5>
+                    <ul>
+                        <li>Acted as Scrum Master on a student development team, overseeing sprint planning, facilitating
+                        stand-ups, and fostering collaboration to enhance project efficiency</li>
+                        <li>Developed backend algorithms in Swift to handle building and in-room navigation from data derived from Apple’s RoomPlan API.</li>
+                        <li>Met with stakeholders weekly to discuss progression and evolving specifications for the end product</li>
+                        <li>As a team, successfully created a full stack IOS app prototype that will be deployed to production in the future</li>
+                        
+                    </ul>
+                    
+                </div>
+                <div className="timeline-item">
+                    <div className="tl-icon">
+                        <FaBriefcase/>
+                    </div>
+                    <p className="tl-duration">
+                        June 2023 - August 2023
+                    </p>
+                    <h5>Software Engineer Intern<span> - Kohl's Technology</span></h5>
+                    <ul>
+                        <li>Worked on the Rewards Team, which oversees the Kohl's Rewards Program and Credit Card</li>
+                        <li>Built Java Spring proudction apps that rewarded bonus Kohl's Cash to users, giving ~3 million dollars monthly</li>
+                        <li>Implemented parallelism to speed up month end rewards processes by 80%</li>
+                        <li>Implemented RESTful APi endpoints to reprimand users utilizing the rewards program fraudulently</li>
+                        <li>Leveraged Google Cloud and MongoDB to retrieve and modify reward account data, and implemented Kafka
+                            messaging systems. Deployed applications using Openshift in LLE, Staging, and Prod environments.</li>
+                        
+                    </ul>
+                    
                 </div>
                 <div className="timeline-item">
                     <div className="tl-icon">
@@ -29,44 +65,19 @@ const Timeline = () => {
                         May 2022 - September 2022
                     </p>
                     <h5>The Odin Project</h5>
-                    <p>
-                        The Odin Project is a completely free, open source educational resource for anybody trying to improve
-                        their programming skills. Through The Odin Project, I was taught the complexities of the Ruby programming
-                        language in addition to the 3 frontend languages needed for web development. Much of the knowledge I
-                        gained culminated to the creation of this website! 
-                    </p>
-                </div>
-                <div className="timeline-item">
-                    <div className="tl-icon">
-                        <FaBriefcase/>
-                    </div>
-                    <p className="tl-duration">
-                        February 2021 - March 2022
-                    </p>
-                    <h5>Student Videographer<span> - UW-Madison CS Department</span></h5>
-                    <p>
-                        Conceptualized and created various promotional content for the school of Computer Sciences, with the end goal of 
-                        creating excitement for the viewer. In addition to this, I collaborated with other videographers and social media managers to assemble the most optimal final product for 
-                        consumption. This was facilitated by discussion in weekly meetings to boost productivity on current projects.
-                    </p>
+                    <ul>
+                        <li>The Odin Project is a completely free, open source educational resource for learning web development</li>
+                        <li>Took self-directed courses that taught beginniner and intermediate level HTML, CSS, and Javascript for web development.</li>
+                        <li>Learned the basics of Ruby, and created various games and activities to bolster knowledge</li>
+                        <li>My knowledge acquired through The Odin Project culminated in the creation of the first iteration of esrig.net, which
+                            can be found on my projects page
+                        </li>
+                        
+                        
+                    </ul>
                     
                 </div>
-                    <div className="timeline-item">
-                        <div className="tl-icon">
-                            <FaBriefcase/>
-                        </div>
-                        <p className="tl-duration">
-                            January 2019 - August 2020
-                        </p>
-                        <h5>Programming Tutor<span> - Code Ninjas</span></h5>
-                        <p>
-                            Code Ninjas is a franchised brick and mortar tutoring location that teaches children programming. As an employee, 
-                            I Structured and facilitated the education of children aged 7-14 in the basics of JavaScript, HTML, and Scratch.
-                            In addition, I Engaged in significant amounts of self-learning of various programming languages, to better understand and 
-                            instruct students. Lastly, I formulated directed lesson plans with other instructors to assist students in better understanding the material, 
-                            leading to greater success. 
-                        </p>
-                    </div>
+                
             </div>
   )
 }
